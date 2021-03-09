252d179bcaca6497855908217194b2cba12b50a8
berlin54 <69752110+berlin54@users.noreply.github.com>
2021-03-09 20:55:57 +0400

    Update Bot Comment.py

--- Bot Comment/Bot Comment.py
+++ Bot Comment/Bot Comment.py
@@ -1,111 +1,99 @@
-#berlin
-#insta : 680068
-
-import requests
-import re
-import os
-import time
-from time import sleep
-
-done = 0
-# - -
-error = 0
-
+try:
+        import requests
+        import sys
+        import time
+        from instabot import Bot
+        import colorama
+        from colorama import Fore
+        colorama.init(autoreset=True)
+except Exception as lib:
+        print(lib)
+        input()
+        sys.exit()
+        
 r = requests.session()
+bot = Bot
+count = 0
 
-banner1 = '''
-  ____   ____ _______    _____ ____  __  __ __  __ ______ _   _ _______ 
- |  _ \ / __ \__   __|  / ____/ __ \|  \/  |  \/  |  ____| \ | |__   __|
- | |_) | |  | | | |    | |   | |  | | \  / | \  / | |__  |  \| |  | |   
- |  _ <| |  | | | |    | |   | |  | | |\/| | |\/| |  __| | . ` |  | |   
- | |_) | |__| | | |    | |____ |__| | |  | | |  | | |____| |\  |  | |   
- |____/ \____/  |_|     \_____\____/|_|  |_|_|  |_|______|_| \_|  |_|
-
-                           [+] By @680068
-                                               
-'''
-
-print(banner1)
+print("""
+      ______ ___________ _     _____ _   _ 
+      | ___ \  ___| ___ \ |   |_   _| \ | |
+      | |_/ / |__ | |_/ / |     | | |  \| |
+      | ___ \  __||    /| |     | | | . ` |
+      | |_/ / |___| |\ \| |_____| |_| |\  |
+      \____/\____/\_| \_\_____/\___/\_| \_/
+""", Fore.GREEN+"\n              ( @680068 & @RRSVC )",Fore.RED+"\n\n       ( This Tool Is Free And Not For Sell )\n")
 
 
-
-user = input('Enter Username :')
-pess = input('Enter Password :')
-url1 = 'https://www.instagram.com/accounts/login/ajax/'
-head1 = {
-	'accept': '*/*',
-	'accept-encoding': 'gzip, deflate, br',
-	'accept-language': 'en-US,en;q=0.9',
-	'content-length': '269',
-	'content-type': 'application/x-www-form-urlencoded',
-	'cookie': 'ig_did=CBE027FC-62A2-4383-85BB-63843B14C94F; ig_nrcb=1; mid=X85qiQALAAFugGOZjugz_zBUZQHx; datr=SD8HYG1W7e-ghmx_vRH1TB7V; fbm_124024574287414=base_domain=.instagram.com; shbid=3563; shbts=1612115664.6497803; rur=VLL; csrftoken=y3yTqqHa0mjqe3ytTy9IBMXw23Z5TT3G',
-	'origin': 'https://www.instagram.com',
-	'referer': 'https://www.instagram.com/',
-	'sec-ch-ua-mobile': '?1',
-	'sec-fetch-dest': 'empty',
-	'sec-fetch-mode': 'cors',
-	'sec-fetch-site': 'same-origin',
-	'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Mobile Safari/537.36',
-	'x-csrftoken': 'y3yTqqHa0mjqe3ytTy9IBMXw23Z5TT3G',
-	'x-ig-app-id': '936619743392459',
-	'x-ig-www-claim': 'hmac.AR2kUU6QD4ZMYLZ-anFcEb7SWiyQTfkx3NQ_KuXrhuCHqUhr',
-	'x-instagram-ajax': '7a3a3e64fa87',
-	'x-requested-with': 'XMLHttpRequest'
-}
-data1 = {
-	'username': f'{user}',
-	'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:&:{pess}'
-}
-log = r.post(url1, headers=head1, data=data1)
-if '"authenticated":true' in log.text:
-	print(f"Done Login {user}")
+username = input('[?] Enter Username :')
+password = input('[?] Enter Password :')
+url_login = 'https://www.instagram.com/accounts/login/ajax/'
+headers_login = {
+        'accept': '*/*',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-length': '259',
+        'content-type': 'application/x-www-form-urlencoded',
+        'cookie': 'ig_did=A8F12857-A12B-4E23-B5D1-B40169809504; mid=YDtfbAALAAE6JNkFQPVp6Xqj9f0s; ig_nrcb=1; shbid=10475; shbts=1614503840.6921477; rur=FTW; csrftoken=T2tmvgY4ZcVStZWf2t2kFg5SkLMyeCdf',
+        'origin': 'https://www.instagram.com',
+        'referer': 'https://www.instagram.com/',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36',
+        'x-csrftoken': 'T2tmvgY4ZcVStZWf2t2kFg5SkLMyeCdf',
+        'x-ig-app-id': '936619743392459',
+        'x-ig-www-claim': 'hmac.AR2zRYl4QYaWW3AluGpM9gi7a8iKcQSNEpfuwqOqpEcqpuio',
+        'x-instagram-ajax': '0edc1000e5e7',
+        'x-requested-with': 'XMLHttpRequest',
+        }
+data_login = {
+        'username': f'{username}',
+        'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:&:{password}'
+        }
+r1 = r.post(url_login, headers=headers_login, data=data_login)
+if '"authenticated":true' in r1.text:
+        print(Fore.GREEN+f"[+] Logged In >> @{username}")
 else:
-	print(f'Error Login {user} Try again later ')
-	print(" ")
-	input("Press Enter To Close Program ...")
-sessi = log.cookies['sessionid']
-post_url = input("Post Url :")
-post_id_url = post_url+'?__a=1'
-try_to_get_post_id = r.get(post_id_url).text
-pst = re.findall('"GraphImage","id":"(.*?)"', try_to_get_post_id)
-post_id = " ".join(pst)
-url2 = f'https://www.instagram.com/web/comments/{post_id}/add/'
-head2 = {
-    'accept': '*/*',
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'en-US,en;q=0.9',
-    'content-length': '40',
-    'content-type': 'application/x-www-form-urlencoded',
-    'cookie': 'ig_did=CBE027FC-62A2-4383-85BB-63843B14C94F; ig_nrcb=1; mid=X85qiQALAAFugGOZjugz_zBUZQHx; datr=SD8HYG1W7e-ghmx_vRH1TB7V; fbm_124024574287414=base_domain=.instagram.com; shbid=3563; shbts=1612115664.6497803; rur=VLL; csrftoken=wAMF4pT7fjbq9ewLB8bmf8cQNZUa3VlX; ds_user_id=45286827132; sessionid='+sessi,
-    'origin': 'https://www.instagram.com',
-    'referer': 'https://www.instagram.com/p/CKuC44sLbBI/comments/',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Mobile Safari/537.36',
-    'x-csrftoken': 'wAMF4pT7fjbq9ewLB8bmf8cQNZUa3VlX',
-    'x-ig-app-id': '1217981644879628',
-    'x-ig-www-claim': 'hmac.AR2kUU6QD4ZMYLZ-anFcEb7SWiyQTfkx3NQ_KuXrhuCHqUhr',
-    'x-instagram-ajax': '7a3a3e64fa87',
-    'x-requested-with': 'XMLHttpRequest'
-}
-com = input('Enter Comment :')
-slp = int(input('Enter Sleep :'))
-data2 = {
-    'comment_text': f'{com}',
+        print(Fore.RED+f'[-] Failed Login >> @{username}')
+        input()
+        sys.exit()
+sessionid = r1.cookies['sessionid']
+url = input('[?] Enter Post Url :')
+post_id = bot.get_media_id_from_link(self='',link=url)
+url_comment = f'https://www.instagram.com/web/comments/{post_id}/add/'
+headers_comment = {
+        'accept': '*/*',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-length': '42',
+        'content-type': 'application/x-www-form-urlencoded',
+        'cookie': 'ig_did=A8F12857-A12B-4E23-B5D1-B40169809504; mid=YDtfbAALAAE6JNkFQPVp6Xqj9f0s; ig_nrcb=1; shbid=10475; shbts=1614503840.6921477; csrftoken=Iytc0tyd1BTP4vRe1pi8caxVBLqPehpp; ds_user_id=434977737; sessionid='+sessionid,
+        'origin': 'https://www.instagram.com',
+        'referer': 'https://www.instagram.com/p/CL4c1-BBUD8/',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36',
+        'x-csrftoken': 'Iytc0tyd1BTP4vRe1pi8caxVBLqPehpp',
+        'x-ig-app-id': '936619743392459',
+        'x-ig-www-claim': 'hmac.AR2zRYl4QYaWW3AluGpM9gi7a8iKcQSNEpfuwqOqpEcqpgGf',
+        'x-instagram-ajax': '0edc1000e5e7',
+        'x-requested-with': 'XMLHttpRequest',
+        }
+comment = input('[?] Enter Comment :')
+sleep = int(input('[?] Enter Sleep :'))
+data_comment = {
+    'comment_text': f'{comment}',
     'replied_to_comment_id': ''
 }
 while True:
-	pcom = r.post(url2, headers=head2, data=data2)
-	if ('"status":"ok"') in pcom.text:
-                done +=1
-                os.system('cls' if os.name == 'nt' else 'clear')
-                print(f'{banner1}\n- BOT COMMENT STARTING -\n\n=============================\n\n[-] Done Send Comment : {done}\n\n[-] Error Send Comment : {error}\n\n=============================')
-                sleep(slp)
-                
+	r2 = r.post(url_comment, headers=headers_comment, data=data_comment)
+	if '"status":"ok"' in r2.text:
+		count +=1
+		print(Fore.GREEN+f"[{count}] Done Send Comment")
+		time.sleep(sleep)
 	else:
-                error +=1
-                os.system('cls' if os.name == 'nt' else 'clear')
-                print(f'{banner1}\n- BOT COMMENT STARTING -\n\n=============================\n\n[-] Done Send Comment : {done}\n\n[-] Error Send Comment : {error}\n\n=============================')
-                sleep(slp)
-                
+		count +=1
+		print(Fore.RED+f"[{count}] Error Send Comment")
+		time.sleep(sleep)
